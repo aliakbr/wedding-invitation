790a982118cc10fa0dc728e0851756baf14e5bdf
Ali Akbar <aliakbar@Alis-MacBook-Pro-2.local>
2024-04-07 12:23:39 +0700

    finish web

--- js/app.js
+++ js/app.js
@@ -1,27 +1,17 @@
+var music = new Audio('music/love-letter.mp3');
+var state = 1; // 0 : Played ; 1: Muted
+
+function playMusic(){
+    if (state == 0) {
+        music.play();
+    } else {
+        music.pause();
+    }
+}
+
 $('#open-invitation').click(function (){
     $('#overlay').addClass('hide-overlay');
-    playAudio1();
-    /*yang bagian floating button ada animasinya tapi karena ketutup dulu sama overlay jadinya gak nampak*/
-    /*pengen pas di klik buka undangan baru jalan animasinya*/
-    /*var c = document.getElementsByclass('right-sidebar');
-    for (var i = 0; i < c.length; i++) {
-        c[i].classList.add('animate');
-    }*/
-});
-
-// List of testimonials
-$.get( "https://api.github.com/repos/ngodingsolusi/the-wedding-of-rehan-maulidan/issues?state=all&per_page", function( data ) {
-    for (let i = 0; i < data.length; i++){
-        const item = `
-            <a class="list-group-item">
-                <div class="name"><span class="strong" style="font-size: 24px">${data[i].user.login}</span> <span class="label label-success">${data[i].labels.length > 0 ? data[i].labels[0].name : 'Tanpa Keterangan'}</span></div>
-                <hr style="margin-top: 0;">
-                <div class="comment">${new showdown.Converter().makeHtml(data[i].body)}</div>
-            </a>
-        `;
-
-        $("#fh5co-testimonial .list-group").append(item);
-    }
+    playMusic();
 });
 
 function scrollToElement(el) {
--- js/location.js
+++ js/location.js
@@ -1,19 +1,7 @@
 function akadLocation(){
-    // If it's an iPhone..
-    if( (navigator.platform.indexOf("iPhone") != -1)
-            || (navigator.platform.indexOf("iPod") != -1)
-            || (navigator.platform.indexOf("iPad") != -1))
-        window.open("maps://www.google.com/maps?daddr=masjid+keuchik+leumik");
-    else
-        window.open("https://www.google.com/maps?daddr=masjid+keuchik+leumik");
+    window.open("https://www.google.com/maps?daddr=masjid+keuchik+leumik");
 }
 
-function walimahLocation(){
-    // If it's an iPhone..
-    if( (navigator.platform.indexOf("iPhone") != -1)
-            || (navigator.platform.indexOf("iPod") != -1)
-            || (navigator.platform.indexOf("iPad") != -1))
-        window.open("maps://www.google.com/maps?daddr=Amel+Convention+Hall");
-    else
-        window.open("https://www.google.com/maps?daddr=Amel+Convention+Hall");
+function receptionLocation(){
+    window.open("https://www.google.com/maps?daddr=masjid+keuchik+leumik");
 }
